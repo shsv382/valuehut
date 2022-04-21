92ffe514b467a021c7c6b9605bc5db092a180b5b
shsv382 <shsv382@gmail.com>
2022-04-22 01:14:03 +0300

    Fixed .gitignore

---
+++ build/static/js/498.93385c83.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdacht=self.webpackChunkdacht||[]).push([[498],{498:function(a,e,c){c.r(e),c.d(e,{default:function(){return t}});c(791);var n=c(184),t=function(){return(0,n.jsx)("div",{className:"page contact-page appearancable",children:(0,n.jsx)("h1",{children:"Contact Page"})})}}}]);
+//# sourceMappingURL=498.93385c83.chunk.js.map
