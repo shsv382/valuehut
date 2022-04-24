a6ae817f6d78c0b6aef623c13a2f279233e4f0fe
shsv382 <shsv382@gmail.com>
2022-04-24 18:10:14 +0300

    Removed logger to push to heroku

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './root-reducer';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
-const middlewares = [];
+// const middlewares = [];
 
-if(process.env.NODE_ENV === 'development') {
-    middlewares.push(logger);
-}
+// if(process.env.NODE_ENV === 'development') {
+//     middlewares.push(logger);
+// }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-// export const store = createStore(rootReducer);
+// export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer);
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
