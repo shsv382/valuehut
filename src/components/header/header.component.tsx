99f66d7a46fee029bff65dccf5cbc2c40a03e11d
shsv382 <shsv382@gmail.com>
2022-05-21 12:26:43 +0300

    Fixed header, trustpilot removed from consultancy and coaching pages

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.styles.scss';
 
 import LogoContainer from '../logo-container/logo-container.component';
@@ -7,8 +7,16 @@ import CartButtonContainer from '../cart-button-container/cart-button-container.
 import PopupMenuTraining from '../popup-menu-training/popup-menu-training.component';
 
 const Header: React.FC = () => {
+    const [scrolled, setScrolled] = useState(false);
+    window.onscroll = () => {
+        if(document.documentElement.scrollTop > 80) {
+            setScrolled(true)
+        } else {
+            setScrolled(false)
+        }
+    };
     return (
-        <header>
+        <header style={{height: scrolled ? 80 : 120, transition: "all 0.25s"}}>
             <div className="header-container" id="start">
                 <LogoContainer imageURL='./logo/logo-primary.png' />
                 <NavbarContainer />
