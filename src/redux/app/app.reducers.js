9175cfba2e1fd90e3b93379d339aae19cfa7eb48
shsv382 <shsv382@gmail.com>
2022-05-22 21:49:41 +0300

    Added modal window for several markets

--- src/redux/app/app.reducers.js
+++ src/redux/app/app.reducers.js
@@ -1,6 +1,7 @@
 import AppActionTypes from './app.types.js';
 
 const INITIAL_STATE = {
+    modal_hidden: false,
     region: 'United Kingdom',
     showPopupMenuTraining: true
 }
@@ -11,6 +12,11 @@ const appReducer = (state=INITIAL_STATE, action) => {
             return {
                 ...state,
                 region: action.payload
+            }
+        case AppActionTypes.TOGGLE_MODAL_HIDDEN:
+            return {
+                ...state,
+                modal_hidden: !state.modal_hidden
             }
         case AppActionTypes.SHOW_POPUP_MENU_TRAINING:
             return {
