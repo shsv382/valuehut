2d32e8dc05c3143df3e4634417ca26405042c2fb
Сергей Шабанов <shsv382@gmail.com>
2022-06-04 23:44:32 +0300

    BrowserRouter replaced to HashRouter fir gh-pages deploy

--- src/index.tsx
+++ src/index.tsx
@@ -7,16 +7,16 @@ import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
